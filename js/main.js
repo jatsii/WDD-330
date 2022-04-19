fc2164f382d3f4209cd9130aebaec2c98799d6a0
Jatsiri Martinez <65254454+jatsii@users.noreply.github.com>
2022-04-19 16:10:29 -0500

    add dynamic index

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const links = [
+    {
+      label: "Week1 notes",
+      url: "week01/index.html"
+    }
+  ]
+
+  function loadIndex()
+  {
+    const ol =  document.querySelector("#linksList");
+
+    links.forEach( link => {
+        const li = document.createElement("li");
+        const href = document.createElement("a");
+        href.setAttribute("href", link.url);
+        href.innerText = link.label;
+
+        li.appendChild(href);
+        ol.appendChild(li);
+
+    })
+  }
